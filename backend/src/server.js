033912bb3ba276b9351bae45a56cbf65692b194a
Tahir Pathan <tahir.pathan88@gmail.com>
2026-04-18 12:21:36 +0530

    Fix DB connection and debug register

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const io = new Server(server,{
 
 chatSocket(io);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=>{
     console.log(`Server Running on Port ${PORT}`)
